b48403f0fdfbdba48acf462082673a43460ab4f1
Anastasia-Rus <121459807+Anastasia-Rus@users.noreply.github.com>
2023-01-25 22:32:02 +0300

    Добавали метод 2

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ using static System.Console;
 
 Clear();
 
-string[] method1(string[] input, int x) {
-    string[] output = new string[C(input, [x])];
+string[] Method1(string[] input, int x) {
+    string[] output = new string[C(input,[x])];
 
     for(int i = 0, j = 0; i < input.Length; i++) {
         if(input[i].Length <= x) {
@@ -15,4 +15,16 @@ string[] method1(string[] input, int x) {
 
     return output;
 }
+int Method2(string[] input, int x) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= x) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
 
