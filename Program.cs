5c281f7994d3abb4d8e81e90bda0b4a867b87626
Anastasia-Rus <121459807+Anastasia-Rus@users.noreply.github.com>
2023-01-25 22:05:17 +0300

    Создали первый метод

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string[] method1(string[] input, int x) {
+    string[] output = new string[C(input, [x])];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= x) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
